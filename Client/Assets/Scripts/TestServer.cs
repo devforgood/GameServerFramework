7e9c276d6f837a392ef038562335f6a39b82e7ac
song jun ho <devforgood@gmail.com>
2021-04-22 22:48:02 +0900

    클릭 이벤트 소스 정리

--- Client/Assets/Scripts/TestServer.cs
+++ Client/Assets/Scripts/TestServer.cs
@@ -29,9 +29,13 @@ public class TestServer : MonoBehaviour
 	int message_count = 0;
 	float lastSendTime = 0f;
 	int seq = 1;
+
+
+	RaycastHit hit_;
 	bool clickOn = false;
 	bool clickCtrlOn = false;
 	bool clickAltOn = false;
+	bool clickLeftOn = false;
 
 	// Start is called before the first frame update
 	void Start()
@@ -204,6 +208,25 @@ public class TestServer : MonoBehaviour
 	{
 	}
 
+	bool GetHitPoint()
+	{
+		Vector3 mos = Input.mousePosition;
+		mos.z = camera.farClipPlane; // 카메라가 보는 방향과, 시야를 가져온다.
+
+		Vector3 dir = camera.ScreenToWorldPoint(mos);
+		// 월드의 좌표를 클릭했을 때 화면에 자신이 보고있는 화면에 맞춰 좌표를 바꿔준다.
+
+		if (Physics.Raycast(camera.transform.position, dir, out hit_, mos.z))
+		{
+			//target.position = hit.point; // 타겟을 레이캐스트가 충돌된 곳으로 옮긴다.
+			if (hit_.transform.gameObject.tag == "floor")
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
 	// Update is called once per frame
 	void Update()
     {
@@ -234,99 +257,62 @@ public class TestServer : MonoBehaviour
 			clickAltOn = false;
         }
 
-		if (Input.GetMouseButton(0))  // 마우스가 클릭 되면
+		if (Input.GetMouseButtonUp(1))
+		{
+			clickLeftOn = false;
+		}
+
+
+
+		if (Input.GetMouseButton(0) && Input.GetKey(KeyCode.LeftControl))  
+		{
+			if (clickCtrlOn == false)
+			{
+				if (GetHitPoint())
+				{
+					SendMessage(MakeAddAgent(hit_.point)); 
+                }
+
+				clickCtrlOn = true;
+            }
+        }
+		else if (Input.GetMouseButton(0) && Input.GetKey(KeyCode.LeftAlt))
+		{
+			if (clickAltOn == false)
+			{
+				if (GetHitPoint())
+				{
+
+				}
+				clickAltOn = true;
+			}
+		}
+		else if (Input.GetMouseButton(0))
 		{
 			if (clickOn == false)
 			{
-				Vector3 mos = Input.mousePosition;
-				mos.z = camera.farClipPlane; // 카메라가 보는 방향과, 시야를 가져온다.
-
-				Vector3 dir = camera.ScreenToWorldPoint(mos);
-				// 월드의 좌표를 클릭했을 때 화면에 자신이 보고있는 화면에 맞춰 좌표를 바꿔준다.
-
-				RaycastHit hit;
-				if (Physics.Raycast(camera.transform.position, dir, out hit, mos.z))
+				if (GetHitPoint())
 				{
-					//target.position = hit.point; // 타겟을 레이캐스트가 충돌된 곳으로 옮긴다.
-					if (hit.transform.gameObject.tag == "floor")
-					{
-						SendMessage(MakeSetMoveTarget(-1, hit.point));
-					}
+					SendMessage(MakeSetMoveTarget(-1, hit_.point));
 				}
 
 				clickOn = true;
 			}
 		}
 
-		if (Input.GetMouseButton(0) && Input.GetKey(KeyCode.LeftControl))  // 마우스가 클릭 되면
+		if (Input.GetMouseButton(1))  
 		{
-			if (clickCtrlOn == false)
+			if (clickLeftOn == false)
 			{
-                Vector3 mos = Input.mousePosition;
-                mos.z = camera.farClipPlane; // 카메라가 보는 방향과, 시야를 가져온다.
-
-                Vector3 dir = camera.ScreenToWorldPoint(mos);
-                // 월드의 좌표를 클릭했을 때 화면에 자신이 보고있는 화면에 맞춰 좌표를 바꿔준다.
-
-                RaycastHit hit;
-                if (Physics.Raycast(camera.transform.position, dir, out hit, mos.z))
-                {
-                    //target.position = hit.point; // 타겟을 레이캐스트가 충돌된 곳으로 옮긴다.
-                    if (hit.transform.gameObject.tag == "floor")
-                    {
-                        SendMessage(MakeAddAgent(hit.point));
-                    }
-                }
-
-				clickCtrlOn = true;
-            }
-        }
-
-		if (Input.GetMouseButton(1))  // 마우스가 클릭 되면
-		{
-			if (clickCtrlOn == false)
-			{
-				Vector3 mos = Input.mousePosition;
-				mos.z = camera.farClipPlane; // 카메라가 보는 방향과, 시야를 가져온다.
-
-				Vector3 dir = camera.ScreenToWorldPoint(mos);
-				// 월드의 좌표를 클릭했을 때 화면에 자신이 보고있는 화면에 맞춰 좌표를 바꿔준다.
-
-				RaycastHit hit;
-				if (Physics.Raycast(camera.transform.position, dir, out hit, mos.z))
+				if (GetHitPoint())
 				{
-					//target.position = hit.point; // 타겟을 레이캐스트가 충돌된 곳으로 옮긴다.
-					if (hit.transform.gameObject.tag == "floor")
-					{
-						SendMessage(MakeSetRaycast(hit.point));
-					}
+					SendMessage(MakeSetRaycast(hit_.point));
 				}
 
-				clickCtrlOn = true;
+				clickLeftOn = true;
 			}
 		}
 
-		if (Input.GetMouseButton(0) && Input.GetKey(KeyCode.LeftAlt))  // 마우스가 클릭 되면
-		{
-			if (clickAltOn == false)
-			{
-                Vector3 mos = Input.mousePosition;
-                mos.z = camera.farClipPlane; // 카메라가 보는 방향과, 시야를 가져온다.
 
-                Vector3 dir = camera.ScreenToWorldPoint(mos);
-                // 월드의 좌표를 클릭했을 때 화면에 자신이 보고있는 화면에 맞춰 좌표를 바꿔준다.
-
-                RaycastHit hit;
-                if (Physics.Raycast(camera.transform.position, dir, out hit, mos.z))
-                {
-                    //target.position = hit.point; // 타겟을 레이캐스트가 충돌된 곳으로 옮긴다.
-                    if (hit.transform.gameObject.tag == "floor")
-                    {
-
-                    }
-                }
-				clickAltOn = true;
-            }
-        }
 	}
 }
