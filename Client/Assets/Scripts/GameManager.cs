413cba14aa80ffba5c76c8ad77d1c77da3ac01e1
denny.song <denny.song@friendsgames.com>
2021-04-27 12:39:43 +0900

    게임 매니저 정리

--- Client/Assets/Scripts/GameManager.cs
+++ Client/Assets/Scripts/GameManager.cs
@@ -6,11 +6,58 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+     public string server_address = "127.0.0.1:65001";
+
+
+
+
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+
+                if (_instance == null)
+                    Debug.Log("no Singleton obj");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
     async Task Start()
     {
         //SRDebug.Init();
+    }
 
+    public void StartGame()
+    {
+
+    }
+
+    public void GameOver()
+    {
+
+    }
+
+    public void BackToMenu()
+    {
 
     }
 }
+
