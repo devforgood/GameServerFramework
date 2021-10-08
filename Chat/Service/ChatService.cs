79e5da23d53b03bc0fba281f2265bea47749c2c7
denny.song <denny.song@friendsgames.com>
2021-10-08 12:06:33 +0900

    chat

--- Chat/Service/ChatService.cs
+++ Chat/Service/ChatService.cs
@@ -28,34 +28,57 @@ namespace Lobby
         {
             var room_id = Guid.NewGuid().ToString();
 
+            var reply1 = new GameChat.ChatMessage()
+            {
+                Code = GameChat.ErrorCode.Success,
+                RoomId = room_id,
+                Message = "welcome",
+            };
+            
+            await responseStream.WriteAsync(reply1);
+
+
             // 조건에 만족하는 유저가 없다면 대기 (redis puh로 활성화)
             var queue = Cache.Instance.GetSubscriber().Subscribe($"room:{room_id}");
 
-            var cts = new CancellationTokenSource();
-            //cts.CancelAfter((int)startplay_polling_period.TotalMilliseconds);
-            try
-            {
-                var ret = await queue.ReadAsync(cts.Token);
 
-                var reply = JsonParser.Default.Parse<GameChat.ChatMessage>(ret.Message);
+            while (true)
+            {
                 try
                 {
-                    await responseStream.WriteAsync(reply);
+                    var ret = await queue.ReadAsync(context.CancellationToken);
+
+                    var reply = JsonConvert.DeserializeObject<GameChat.ChatMessage>(ret.Message);
+                    try
+                    {
+
+                        await responseStream.WriteAsync(reply);
+                        Log.Information($"send {ret.Message}");
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Log.Error($"send error {ret.Message} {ex.ToString()}");
+                        break;
+                    }
                 }
-                catch (InvalidOperationException)
+                catch (OperationCanceledException ex)
                 {
-                    Log.Information($"StartPlay restore match user {ret.Message}");
+                    // cts cancel
+                    Log.Error($"cts error {ex.ToString()}");
+                    break;
                 }
-            }
-            catch (OperationCanceledException)
-            {
-                // cts cancel
             }
         }
 
         public override async Task<global::GameChat.SendChatReply> SendChat(GameChat.SendChatRequest request, ServerCallContext context)
         {
-            var msg = JsonConvert.SerializeObject(request);
+            GameChat.ChatMessage chat_msg = new GameChat.ChatMessage()
+            {
+                RoomId = request.RoomId,
+                Message = request.Message,
+            };
+
+            var msg = JsonConvert.SerializeObject(chat_msg);
             await Cache.Instance.GetSubscriber().PublishAsync($"room:{request.RoomId}", msg);
 
             return new GameChat.SendChatReply()
---
+++ ChatClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using GameService;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("GRPC_TRACE", "api,http,cares_resolver,cares_address_sorting,transport_security,tsi");
+            Environment.SetEnvironmentVariable("GRPC_VERBOSITY", "debug");
+            Grpc.Core.GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+
+
+
+            var channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+            var client = new GameChat.ChatService.ChatServiceClient(channel);
+
+
+
+            int cnt = 0;
+            using (var call = client.CreateRoom(new GameChat.CreateRoomRequest { RoomName = "testroom", SenderName = "testuser" }))
+            {
+                var responseStream = call.ResponseStream;
+                while (await responseStream.MoveNext())
+                {
+                    await client.SendChatAsync(new GameChat.SendChatRequest() { RoomId = responseStream.Current.RoomId, Message = $"test {cnt++}" });
+                }
+            }
+
+            await channel.ShutdownAsync();
+        }
+    }
+}
