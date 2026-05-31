e4d9970d7776e1c8d3ab9062855170371723053f
DENNY_DESKTOP\devfo <devforgood@gmail.com>
2026-06-01 00:28:37 +0900

    퀘스트

---
+++ Game/Quest.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include "syncnet_generated.h"
+
+
+namespace gamedata
+{
+	class Quest; // Forward declaration of gamedata::Quest
+}
+
+class Quest
+{
+public:
+	const gamedata::Quest* gamedata; // Pointer to gamedata for Quest information
+};
+
