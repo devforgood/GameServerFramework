490da8331cdeeb61681359934a83fdff525d1d43
DENNY_DESKTOP\devfo <devforgood@gmail.com>
2026-06-01 01:10:28 +0900

    게임 모드

---
+++ Game/GameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace gamedata
+{
+	class GameMode; // Forward declaration of gamedata::GameMode
+}
+
+
+class GameMode
+{
+public:
+	const gamedata::GameMode* gamedata; // Pointer to gamedata for GameMode information
+};
+
