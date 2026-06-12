6b50204bad71d7e1438a9ac7ace76571104cb1b6
DENNY_DESKTOP\devfo <devforgood@gmail.com>
2026-06-13 00:07:32 +0900

    리소스 로더 수정

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -11,6 +11,8 @@
 
 #include <thread>
 #include <chrono>
+#include <cstdlib>
+#include <cerrno>
 #include <boost/array.hpp>
 
 using namespace std::chrono_literals;
@@ -35,7 +37,18 @@ int main(int argc, char* argv[])
 		std::list<tcp::endpoint> endpoints;
 		for (int i = 1; i < argc; ++i)
 		{
-			tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[i]));
+			char* end = nullptr;
+			errno = 0;
+			long port = std::strtol(argv[i], &end, 10);
+
+			// 숫자로 변환되지 않았거나(빈 문자열/문자 포함), 포트 범위(1~65535)를 벗어나면 거부한다.
+			if (end == argv[i] || *end != '\0' || errno == ERANGE || port < 1 || port > 65535)
+			{
+				std::cerr << "Invalid port: " << argv[i] << " (must be 1-65535)\n";
+				return 1;
+			}
+
+			tcp::endpoint endpoint(tcp::v4(), static_cast<unsigned short>(port));
 			endpoints.push_back(endpoint);
 		}
 
