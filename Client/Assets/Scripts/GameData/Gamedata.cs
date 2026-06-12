7def5420c705d058ad78aaa2d274034775cc74f3
DENNY_DESKTOP\devfo <devforgood@gmail.com>
2026-06-13 00:11:32 +0900

    리소스 로더 수정

--- Client/Assets/Scripts/GameData/Gamedata.cs
+++ Client/Assets/Scripts/GameData/Gamedata.cs
@@ -8,18 +8,18 @@ namespace Gamedata
     [Serializable]
     public class Skill
     {
+        public string code_name;
         public string effect;
         public int hieght;
-        public string type;
-        public string name_id;
-        public int max_damage;
-        public int id;
-        public int range;
-        public int angle;
         public int duration;
+        public int max_damage;
+        public int range;
         public int min_damage;
-        public string code_name;
+        public int id;
         public string desc_id;
+        public string name_id;
+        public string type;
+        public int angle;
     }
 
 
@@ -35,54 +35,30 @@ namespace Gamedata
     public class Item
     {
         public int heal;
-        public string type;
-        public string name_id;
-        public System.Collections.Generic.List<ItemItemOption> item_options;
         public int id;
         public string desc_id;
+        public string name_id;
+        public string type;
+        public System.Collections.Generic.List<ItemItemOption> item_options;
     }
 
 
     [Serializable]
     public class Quest
     {
-        public System.Collections.Generic.List<int> reward_item_ids;
-        public string name_id;
-        public int id;
-        public int objective_count;
         public string code_name;
-        public int reward_exp;
-        public string objective_type;
-        public int objective_target_id;
-        public string type;
         public bool is_repeatable;
+        public string objective_type;
+        public int reward_exp;
         public int level_requirement;
-        public int reward_gold;
+        public int id;
         public string desc_id;
-    }
-
-
-    [Serializable]
-    public class GameModeRules
-    {
-        public int min_level;
-        public int respawn_time;
-        public bool allow_pvp;
-        public string end_condition;
-        public bool has_time_limit;
-        public bool respawn_enabled;
-        public bool allow_trading;
-        public int time_limit;
-        public int max_players;
-    }
-
-
-    [Serializable]
-    public class GameModeRewards
-    {
-        public double drop_rate_multiplier;
-        public double exp_multiplier;
-        public double gold_multiplier;
+        public int reward_gold;
+        public int objective_count;
+        public System.Collections.Generic.List<int> reward_item_ids;
+        public int objective_target_id;
+        public string name_id;
+        public string type;
     }
 
 
@@ -97,89 +73,63 @@ namespace Gamedata
 
 
     [Serializable]
+    public class GameModeRewards
+    {
+        public double exp_multiplier;
+        public double drop_rate_multiplier;
+        public double gold_multiplier;
+    }
+
+
+    [Serializable]
+    public class GameModeRules
+    {
+        public bool has_time_limit;
+        public bool allow_pvp;
+        public bool respawn_enabled;
+        public string end_condition;
+        public int respawn_time;
+        public int time_limit;
+        public bool allow_trading;
+        public int min_level;
+        public int max_players;
+    }
+
+
+    [Serializable]
     public class GameMode
     {
-        public string name;
-        public string name_id;
+        public string category;
+        public GameModeBossInfo boss_info;
         public int id;
-        public GameModeRules rules;
+        public string desc_id;
+        public string name_id;
         public System.Collections.Generic.List<int> maps;
         public GameModeRewards rewards;
-        public string type;
-        public GameModeBossInfo boss_info;
-        public string category;
-        public string desc_id;
-    }
-
-
-    [Serializable]
-    public class MapObjectsMovableObjectPatrolPath
-    {
-        public int y;
-        public int x;
-        public int z;
-    }
-
-
-    [Serializable]
-    public class MapObjectsMovableObjectPosition
-    {
-        public int y;
-        public int x;
-        public int z;
-    }
-
-
-    [Serializable]
-    public class MapObjectsMovableObject
-    {
-        public string type;
         public string name;
-        public int movement_range;
-        public int id;
-        public double movement_speed;
-        public System.Collections.Generic.List<MapObjectsMovableObjectPatrolPath> patrol_path;
-        public MapObjectsMovableObjectPosition position;
-    }
-
-
-    [Serializable]
-    public class MapObjectsStaticObjectSize
-    {
-        public int y;
-        public int x;
-        public int z;
-    }
-
-
-    [Serializable]
-    public class MapObjectsStaticObjectPosition
-    {
-        public int y;
-        public int x;
-        public int z;
-    }
-
-
-    [Serializable]
-    public class MapObjectsStaticObject
-    {
-        public bool collision;
-        public MapObjectsStaticObjectSize size;
+        public GameModeRules rules;
         public string type;
-        public string name;
-        public int id;
-        public int loot_table_id;
-        public int damage;
-        public MapObjectsStaticObjectPosition position;
     }
 
 
     [Serializable]
-    public class MapObjects
+    public class MapGatePosition
     {
-        public System.Collections.Generic.List<MapObjectsMovableObject> movable_objects;
-        public System.Collections.Generic.List<MapObjectsStaticObject> static_objects;
+        public double y;
+        public double x;
+        public double z;
+    }
+
+
+    [Serializable]
+    public class MapGate
+    {
+        public int required_level;
+        public MapGatePosition position;
+        public int target_gate_id;
+        public string name;
+        public int target_map_id;
+        public int id;
     }
 
 
@@ -195,11 +145,31 @@ namespace Gamedata
     [Serializable]
     public class MapSpawnPointsMonsterSpawn
     {
-        public int spawn_delay;
-        public int monster_id;
-        public int boss_id;
         public int spawn_interval;
         public MapSpawnPointsMonsterSpawnPosition position;
+        public int boss_id;
+        public int monster_id;
+        public int spawn_delay;
+    }
+
+
+    [Serializable]
+    public class MapSpawnPointsPlayerSpawnPosition
+    {
+        public double y;
+        public double x;
+        public double z;
+    }
+
+
+    [Serializable]
+    public class MapSpawnPointsPlayerSpawn
+    {
+        public int spawn_interval;
+        public MapSpawnPointsPlayerSpawnPosition position;
+        public int boss_id;
+        public int monster_id;
+        public int spawn_delay;
     }
 
 
@@ -224,31 +194,11 @@ namespace Gamedata
 
 
     [Serializable]
-    public class MapSpawnPointsPlayerSpawnPosition
-    {
-        public double y;
-        public double x;
-        public double z;
-    }
-
-
-    [Serializable]
-    public class MapSpawnPointsPlayerSpawn
-    {
-        public int spawn_delay;
-        public int monster_id;
-        public int boss_id;
-        public int spawn_interval;
-        public MapSpawnPointsPlayerSpawnPosition position;
-    }
-
-
-    [Serializable]
     public class MapSpawnPoints
     {
         public System.Collections.Generic.List<MapSpawnPointsMonsterSpawn> monster_spawn;
-        public System.Collections.Generic.List<MapSpawnPointsBossSpawn> boss_spawn;
         public System.Collections.Generic.List<MapSpawnPointsPlayerSpawn> player_spawn;
+        public System.Collections.Generic.List<MapSpawnPointsBossSpawn> boss_spawn;
     }
 
 
@@ -261,39 +211,89 @@ namespace Gamedata
 
 
     [Serializable]
-    public class MapGatePosition
+    public class MapObjectsStaticObjectPosition
     {
-        public double y;
-        public double x;
-        public double z;
+        public int y;
+        public int x;
+        public int z;
     }
 
 
     [Serializable]
-    public class MapGate
+    public class MapObjectsStaticObjectSize
     {
-        public int required_level;
+        public int y;
+        public int x;
+        public int z;
+    }
+
+
+    [Serializable]
+    public class MapObjectsStaticObject
+    {
+        public MapObjectsStaticObjectPosition position;
+        public bool collision;
+        public string name;
+        public MapObjectsStaticObjectSize size;
+        public int loot_table_id;
+        public int id;
+        public int damage;
+        public string type;
+    }
+
+
+    [Serializable]
+    public class MapObjectsMovableObjectPosition
+    {
+        public int x;
+        public int y;
+        public int z;
+    }
+
+
+    [Serializable]
+    public class MapObjectsMovableObjectPatrolPath
+    {
+        public int y;
+        public int x;
+        public int z;
+    }
+
+
+    [Serializable]
+    public class MapObjectsMovableObject
+    {
+        public MapObjectsMovableObjectPosition position;
+        public System.Collections.Generic.List<MapObjectsMovableObjectPatrolPath> patrol_path;
+        public int movement_range;
         public string name;
         public int id;
-        public int target_gate_id;
-        public int target_map_id;
-        public MapGatePosition position;
+        public string type;
+        public double movement_speed;
+    }
+
+
+    [Serializable]
+    public class MapObjects
+    {
+        public System.Collections.Generic.List<MapObjectsStaticObject> static_objects;
+        public System.Collections.Generic.List<MapObjectsMovableObject> movable_objects;
     }
 
 
     [Serializable]
     public class Map
     {
-        public MapObjects objects;
-        public string name;
-        public string name_id;
         public int id;
-        public string navmesh_path;
-        public int game_mode_id;
-        public MapSpawnPoints spawn_points;
-        public MapSize size;
-        public System.Collections.Generic.List<MapGate> gates;
         public string desc_id;
+        public System.Collections.Generic.List<MapGate> gates;
+        public string navmesh_path;
+        public MapSpawnPoints spawn_points;
+        public int game_mode_id;
+        public string name;
+        public MapSize size;
+        public MapObjects objects;
+        public string name_id;
     }
 
 
